e5f514f6be92bf11adf150d920982da84b4fdfe1
Sanjay Singh <san.singhsanjay@gmail.com>
2019-07-22 18:32:17 +0530

    update Jul-22-2019

--- EnglishVocab/short_test.py
+++ EnglishVocab/short_test.py
@@ -1,17 +1,14 @@
 from termcolor import colored
 from random import shuffle
 
-data = ["Cope#Deal effectively with something difficult.",
-"Jingoistic#Characterized by extreme patriotism, especially in the form of aggressive or war like foreign policy",
-"Mull#Thinking about deeply and at length, warm and add sugar and spices to it",
-"Retribution#punishment/penalty",
-"Endeavour#An attempt to achieve a goal",
-"Misogyny#Dislike of / Contempt for / ingrained prejudice againts women",
-"Pergola#An arched structure in a garden or park consisting of a framework covered with climbing or trailing plants",
-"Retro#Imitative of a style or fashion from the past",
-"Fatigue#Extreme tiredness resulting from mental or physical excertion or illness",
-"Crooked#Bent or twisted out of shape or out of place, dishonest or illegal",
-"Intrigue#Arouse the curiosity or interest of fascinate, Make secret plans to do something illicit or determental to someone"]
+data = ["outlook#a person's point of view or general attitude to life",
+"merriment#gaiety and fun",
+"frisky#playfull and full of energy",
+"hangup#an emotional problem or inhibition",
+"realm#a kingdom, a field / domain of activity / interest",
+"hatred#intense dislike / hate",
+"estuary#the tidal mouth of a large river, a water passage where a tide meets a river current"
+]
 
 count = 0
 shuffle(data)
