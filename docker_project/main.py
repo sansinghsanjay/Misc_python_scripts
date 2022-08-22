4ed2c52f9177cf68e194f9d23a199913a46f465a
Sanjay Singh <san.singhsanjay@gmail.com>
2022-08-22 13:18:12 +0530

    update script

--- docker_project/main.py
+++ docker_project/main.py
@@ -1,1 +1,17 @@
+# packages
+import os
+
+# welcome
 print("Hello to the beautiful and powerful world of Python...")
+
+# get path from user
+path = input("Enter path: ")
+
+# get files from that path
+files = os.listdir(path)
+
+# print the content
+print("Total number of files and / or sub-directories: ", len(files))
+print("Following are the files and / or sub-directories:")
+for i in range(len(files)):
+	print(files[i])
